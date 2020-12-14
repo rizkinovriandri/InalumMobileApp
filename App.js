7e20160ebe30debdbaf87eba0cfbd77b010148af
rizkinovriandri <qieroway@gmail.com>
2020-12-14 22:44:13 +0700

    update fitur

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useWindowDimensions } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import { Splash, Login, Home } from './pages';
 
@@ -13,14 +15,28 @@ const App = () => {
         screenOptions={{
           headerShown: false
         }} 
-        initialRouteName="Landing">
+        initialRouteName="Splash">
         <Stack.Screen name="Splash" component={Splash} />
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={NavDrawerScreen} />
         {/* <Stack.Screen name="Session4" component={Session4} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+const NavDrawer = createDrawerNavigator();
+const NavDrawerScreen = () => (
+  <NavDrawer.Navigator
+      screenOptions={{
+        headerShown: false
+      }} 
+      drawerType='front'
+      initialRouteName="Home">
+    <NavDrawer.Screen name="Home" component={Home} />
+    <NavDrawer.Screen name="Setting" component={Home} />
+    <NavDrawer.Screen name="Payment" component={Home} />
+  </NavDrawer.Navigator>
+);
 
 export default App;
--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
-import { Button, ThemeProvider, Input } from 'react-native-elements';
-import { SafeAreaView, Image, StyleSheet, View, Text } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { ThemeProvider, Tile, PricingCard  } from 'react-native-elements';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import Swiper from 'react-native-swiper';
 
+
+const styles = StyleSheet.create({
+  wrapper: {},
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+    
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5'
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%'
+  },
+
+  overlay: {
+    backgroundColor:'rgba(255,0,0,0.5)',
+  },
+
+  image: {
+    flex: 1
+  }
+})
 
 const Home = ({navigation}) => {
 
@@ -10,7 +52,72 @@ const Home = ({navigation}) => {
     <ThemeProvider>
 
       <SafeAreaView style={styles.container}>
-        <Text>Hello world</Text>
+        
+      <View style={{flex: 1, flexDirection: 'column', paddingTop: 0}}>
+            <View style={{ height: 300, backgroundColor: 'powderblue'}}>
+                <Swiper autoplay style={styles.wrapper} showsButtons={true} activeDotColor="powderblue">
+                  <View style={styles.slide1}>
+                    <Tile
+                      imageSrc={require('../../assets/banner/banner-1.jpg')}
+                      title="Slide1"
+                      featured
+                      caption="Order now"
+                      
+                    />
+                  </View>
+                  <View style={styles.slide2}>
+                    <Tile
+                      imageSrc={require('../../assets/banner/banner-2.jpg')}
+                      title="Slide2"
+                      featured
+                      caption="Order now"
+                    />
+                  </View>
+                  <View style={styles.slide3}>
+                    <Tile
+                      imageSrc={require('../../assets/banner/banner-3.jpg')}
+                      title="Slide3"
+                      featured
+                      caption="Order now"
+                    />
+                  </View>
+                </Swiper>
+            </View>
+
+            <View style={{ flex: 1, flexDirection: 'row', height: 100}}>
+
+                <PricingCard
+                  containerStyle={[{ width:'40%', height: '45%' }]}
+                  titleStyle={[{ fontSize: 12 }]}
+                  pricingStyle={[{ fontSize: 16 }]}
+                  infoStyle={[{ fontSize: 12 }]}
+                  button={[{ fontSize: 12 }]}
+                  color="#4f9deb"
+                  title="Free"
+                  price="$0"
+                  info={['1 User', 'Basic Support']}
+                  button={{ title: ' Order now', icon: 'flight-takeoff' }}
+                />
+
+                <PricingCard
+                  containerStyle={[{ width:'40%', height: '45%' }]}
+                  titleStyle={[{ fontSize: 12 }]}
+                  pricingStyle={[{ fontSize: 16 }]}
+                  infoStyle={[{ fontSize: 12 }]}
+                  button={[{ fontSize: 12 }]}
+                  color="#4f9deb"
+                  title="Paid"
+                  price="$10"
+                  info={['1 User', 'Basic Support']}
+                  button={{ title: ' Order now', icon: 'flight-takeoff' }}
+                />
+
+            </View>
+
+
+
+            
+          </View>
         
       </SafeAreaView>
 
@@ -18,14 +125,5 @@ const Home = ({navigation}) => {
     </ThemeProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    width: '100%'
-  }
-
-})
 
 export default Home;
